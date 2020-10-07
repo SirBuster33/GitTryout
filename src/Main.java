65c8252af5c37b2f0e3758b7ba9d5ab44bd11759
SirBuster33 <sirbuster333@gmail.com>
2020-10-07 13:00:40 +0200

    // Do we have a conflict now?

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         // a comment
-        System.out.println("Ey, I am back. Do we have a conflict?");
+        System.out.println("Ey, I am back. Do we have a conflict?"); // Do we have a conflict now?
     }
 }
